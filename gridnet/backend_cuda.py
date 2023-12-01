c71d64f9e48342ca843b2aa847fa8b892ef41e96
Alex Nichol <unixpickle@gmail.com>
2023-11-30 20:23:11 -0500

    start testing CUDA kernels

---
+++ gridnet/backend_cuda.py
@@ -0,0 +1,58 @@
+import gridnet_cuda
+import torch
+
+
+class GridnetCudaOp(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx,
+        weight: torch.Tensor,
+        bias: torch.Tensor,
+        init_activations: torch.Tensor,
+        inner_iters: int,
+        block_size: int,
+        eps: float,
+    ):
+        batch_size, m, n, k = init_activations.shape
+        assert (
+            m % block_size == 0 and n % block_size == 0 and k % block_size == 0
+        ), f"{block_size=} incompatible with {init_activations.shape=}"
+        assert weight.shape == (
+            3**3,
+            *init_activations.shape[1:],
+        ), f"{weight.shape=} invalid for {init_activations.shape=}"
+        assert (
+            bias.shape == weight.shape[1:]
+        ), f"{bias.shape=} invalid for {weight.shape=}"
+        if not batch_size:
+            return init_activations.clone()
+        ctx.save_for_backward(weight, bias, init_activations)
+        ctx.inner_iters = inner_iters
+        ctx.block_size = block_size
+        ctx.eps = eps
+        outputs = torch.empty_like(init_activations)
+
+        gridnet_cuda.forward(
+            weight, bias, init_activations, outputs, inner_iters, block_size, eps
+        )
+        return outputs
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (weight, bias, init_activations) = ctx.saved_tensors
+        weight_grad = torch.empty_like(weight)
+        bias_grad = torch.empty_like(bias)
+        init_activations_grad = torch.empty_like(init_activations)
+        gridnet_cuda.backward(
+            weight,
+            bias,
+            init_activations,
+            grad_output,
+            weight_grad,
+            bias_grad,
+            init_activations_grad,
+            ctx.inner_iters,
+            ctx.block_size,
+            ctx.eps,
+        )
+        return weight_grad, bias_grad, init_activations, None, None, None
--- gridnet/backend_cuda_test.py
+++ gridnet/backend_cuda_test.py
@@ -16,13 +16,40 @@ from gridnet.backend_torch import outer_step_pytorch
 )
 def test_forward_equivalence(shape: Tuple[int, int, int], block_size: int):
     # Import must come after `import torch` to avoid linking issues
-    import gridnet_cuda
+    from gridnet.backend_cuda import GridnetCudaOp
 
     eps = 1e-5
     inputs = torch.randn(2, *shape).cuda()
     weights = torch.randn(3**3, *shape).cuda()
     biases = torch.randn(*shape).cuda()
     expected = outer_step_pytorch(weights, biases, inputs, 2, block_size, eps)
-    actual = torch.zeros_like(expected)
-    gridnet_cuda.forward(weights, biases, inputs, actual, 2, block_size, eps)
+    actual = GridnetCudaOp.apply(weights, biases, inputs, 2, block_size, eps)
     assert (actual - expected).abs().max().item() < 1e-4
+
+
+@pytest.mark.parametrize(
+    "shape,block_size",
+    (
+        ((16, 16, 16), 8),
+        ((32, 64, 128), 8),
+        ((32, 64, 128), 4),
+    ),
+)
+def test_grad_equivalence(shape: Tuple[int, int, int], block_size: int):
+    # Import must come after `import torch` to avoid linking issues
+    from gridnet.backend_cuda import GridnetCudaOp
+
+    eps = 1e-5
+    inputs = torch.randn(2, *shape).cuda().requires_grad_(True)
+    weights = torch.randn(3**3, *shape).cuda().requires_grad_(True)
+    biases = torch.randn(*shape).cuda().requires_grad_(True)
+    expected = outer_step_pytorch(weights, biases, inputs, 2, block_size, eps)
+    out_grad = torch.randn_like(expected).cuda()
+    expected_grads = torch.autograd.grad(expected, (biases, weights, inputs), out_grad)
+    actual = GridnetCudaOp.apply(weights, biases, inputs, 2, block_size, eps)
+    actual_grads = torch.autograd.grad(actual, (biases, weights, inputs), out_grad)
+    for name, x, a in zip(
+        ["biases", "weights", "inputs"], expected_grads, actual_grads
+    ):
+        err = (x - a).abs().max().item()
+        assert err < 1e-4, f"MAE in {name}: {err}"
