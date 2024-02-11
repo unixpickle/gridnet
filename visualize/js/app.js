8ed1f34fe7b99452153ef3c53bf0f47440315943
Alex Nichol <unixpickle@gmail.com>
2024-02-11 13:07:27 -0500

    nicer looking predictions

--- visualize/js/app.js
+++ visualize/js/app.js
@@ -119,12 +119,46 @@ class ImagePicker {
     }
 }
 ImagePicker.PADDING = 20;
+class Predictions {
+    constructor() {
+        this.element = document.getElementById('predictions');
+    }
+    showPredictions(probs) {
+        const classes = [];
+        for (let i = 0; i < probs.data.length; i++) {
+            classes.push([probs.data[i], window.ImagenetClasses[i]]);
+        }
+        classes.sort((a, b) => b[0] - a[0]);
+        this.element.innerHTML = '';
+        classes.slice(0, 10).forEach((probAndCls) => {
+            const name = document.createElement('label');
+            name.textContent = probAndCls[1];
+            name.className = 'predictions-row-label';
+            const percentText = (probAndCls[0] * 100).toFixed(2) + '%';
+            const probBar = document.createElement('div');
+            probBar.className = 'predictions-row-prob-bar';
+            const probBarInner = document.createElement('div');
+            probBarInner.className = 'predictions-row-prob-bar-inner';
+            probBarInner.style.width = percentText;
+            probBar.appendChild(probBarInner);
+            const probLabel = document.createElement('label');
+            probLabel.textContent = percentText;
+            probLabel.className = 'predictions-row-prob-label';
+            probBar.appendChild(probLabel);
+            const row = document.createElement('div');
+            row.className = 'predictions-row';
+            row.appendChild(name);
+            row.appendChild(probBar);
+            this.element.appendChild(row);
+        });
+    }
+}
 class App {
     constructor() {
         this.readyToClassify = false;
         this.imagePicker = new ImagePicker();
         this.classifyButton = document.getElementById('classify-button');
-        this.predictions = document.getElementById('predictions');
+        this.predictions = new Predictions();
         loadModel().then((model) => {
             this.model = model;
             if (this.readyToClassify) {
@@ -141,22 +175,7 @@ class App {
             const img = this.imagePicker.getImage();
             const pred = this.model.forward(img);
             const probs = softmax(pred);
-            const classes = [];
-            for (let i = 0; i < probs.data.length; i++) {
-                classes.push([probs.data[i], window.ImagenetClasses[i]]);
-            }
-            classes.sort((a, b) => b[0] - a[0]);
-            this.predictions.innerHTML = '';
-            classes.slice(0, 10).forEach((probAndCls) => {
-                const item = document.createElement('div');
-                const name = document.createElement('label');
-                name.textContent = probAndCls[1];
-                const probLabel = document.createElement('label');
-                probLabel.textContent = '' + probAndCls[0];
-                item.appendChild(name);
-                item.appendChild(probLabel);
-                this.predictions.appendChild(item);
-            });
+            this.predictions.showPredictions(probs);
         });
     }
 }
